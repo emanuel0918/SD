396f5f14d426d84ca0ffa4b1c87dfee9a631e76c
Emanuel <emanuelbarrera98@gmail.com>
2020-04-14 17:21:54 +0200

    SD

---
+++ pruebas/01/p1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+int main(void){
+ struct sockaddr_in direccionServidor;
+ direccionServidor.sin_family=AF_INET;
+ direccionServidor.sin_addr.s_addr=INADDR_ANY;
+ direccionServidor.sin_port=htons(8080);
+ int servidor=socket(AF_INET,SOCK_STREAM,0);
+ if(bind(servidor,(void*)&direccionServidor,sizeof(direccionServidor))!=0){
+  perror("Fallo el bind\n");
+  return 1;
+ }
+ printf("Estoy escuchando\n");
+ for(;;);
+ return 0;
+}
