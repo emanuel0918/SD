fe6c0cec0d14d87398779a5129fd514feb8620f1
Emanuel <emanuelbarrera98@gmail.com>
2020-06-07 22:19:19 +0200

    SD

--- zerocopy/libzerocopyMQ/libzerocopyMQ.c
+++ zerocopy/libzerocopyMQ/libzerocopyMQ.c
@@ -4,10 +4,10 @@
 int create_destroy(const char * cola, char opc){
 
 	int s=obtenerSocket();
-	int i;
+	//int i;
 	char respuesta[4];
 	respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-	int leido;
+	//int leido;
 	char *opcion=(char*)malloc(2*sizeof(char));
 	char *sizeof_cola_s=intToString(strlen(cola));
 	opcion[0]=opc;opcion[1]='\0';
@@ -86,7 +86,7 @@ int put(const char *cola, const void *mensaje, uint32_t tam) {
 int get(const char *cola, void **mensaje, uint32_t *tam, bool blocking) {
 
 	int s=obtenerSocket();
-	int i;
+	//int i;
 	char sizeof_mensaje_s[TAM_LONG];
 	char respuesta[4];
 	respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
@@ -123,19 +123,23 @@ int get(const char *cola, void **mensaje, uint32_t *tam, bool blocking) {
 			if(sizeof_mensaje_s[0]!='\000'){ 
 				int sizeof_mensaje= atoi(sizeof_mensaje_s);
 				sizeof_mensaje+=1;
+				/*
 				char mensaje_s[sizeof_mensaje];
 				for( i=0;i<sizeof_mensaje;i++){
 					mensaje_s[i]='\0';
 				}
+				*/
 				*mensaje=(char*)malloc(sizeof_mensaje*sizeof(char));
 				*tam=(uint32_t )sizeof_mensaje-2;
 				//printf("sizeof_mensaje : %d\n",sizeof_mensaje);
-				if((leido=read(s,mensaje_s,sizeof_mensaje)>0)){
+				if((leido=recv(s,((char*)*mensaje),sizeof_mensaje,MSG_WAITALL)>0)){
 					//printf("cadena : %s\n",mensaje_s);
 					//strcpy(*mensaje,mensaje_s);
+					/*
 					for(i=0;i<sizeof_mensaje-2;i++){
 						((char*)*mensaje)[i]=mensaje_s[i];
 					}
+					*/
 					respuesta[0]='0';respuesta[1]='\0';
 					return atoi(respuesta);
 					//mensaje=(void **)&mensaje_s;
