6547d317f03ffaf26ce0c3d0ca26a67466931e99
Emanuel <emanuelbarrera98@gmail.com>
2020-06-07 14:03:38 +0200

    SD

--- zerocopy/libzerocopyMQ/libzerocopyMQ.c
+++ zerocopy/libzerocopyMQ/libzerocopyMQ.c
@@ -1,50 +1,40 @@
 #include <stdint.h>
 #include "zerocopyMQ.h"
 #include "comun.h"
-
-
-
-//---------------------------------------------------------------------------------------------
 int create_destroy(const char * cola, char opc){
 
 	int s=obtenerSocket();
-	//
+	int i;
 	char respuesta[4];
 	respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-	//
 	int leido;
-	//send opc
-	char opcion[2];
-	opcion[0]=opc;opcion[2]='\0';
-	send(s,opcion,2*sizeof(char),0);
-    if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-		//send sizeof
-		char * sizeof_cola_s;
-		sizeof_cola_s=intToString(strlen(cola));
-		send(s,sizeof_cola_s,sizeof(sizeof_cola_s),0);
-    	if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-		//send cola
-		send(s,cola,strlen(cola),0);
-    	if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-			}
-			if (leido<0) {
-					respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-					close(s);
-					return atoi(respuesta);
-			}
-		}
-		if (leido<0) {
-				respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-				close(s);
-				return atoi(respuesta);
-		}
-	}
-	if (leido<0) {
-			respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
+	char *opcion=(char*)malloc(2*sizeof(char));
+	char *sizeof_cola_s=intToString(strlen(cola));
+	opcion[0]=opc;opcion[1]='\0';
+	ssize_t nwritten;
+	struct iovec iov[3];
+	iov[0].iov_base=opcion;
+	iov[0].iov_len=2;
+	iov[1].iov_base=sizeof_cola_s;
+	iov[1].iov_len=TAM_LONG;
+	/*
+	 AQUI EL TAMANIO VARIABLE DE LA COLA
+	*/
+	iov[2].iov_base=cola;
+	iov[2].iov_len=atoi(sizeof_cola_s)+1;
+	/*
+	AQUI SOLAMENTE HE ENVIADO UNA TRANSMISION
+	*/
+	/*
+	iov[3].iov_base="0\0";
+	iov[3].iov_len=2;
+	*/
+	while((nwritten=writev(s,iov,3))>0){
+		if(read(s,respuesta,sizeof(respuesta))>0){
 			close(s);
 			return atoi(respuesta);
+		}
 	}
-	//
 	return atoi(respuesta);
 }
 int createMQ(const char *cola) {
@@ -57,71 +47,45 @@ int destroyMQ(const char *cola){
 int put(const char *cola, const void *mensaje, uint32_t tam) {
 
 	int s=obtenerSocket();
-	//
+	char *opcion=(char*)malloc(2*sizeof(char));
+	char *sizeof_cola_s=intToString(strlen(cola));
+	opcion[0]='p';opcion[1]='\0';
 	char respuesta[4];
-	respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-	//
-	int leido;
-	//send opc
-	char opcion[2];
-	opcion[0]='p';opcion[2]='\0';
-	send(s,opcion,2*sizeof(char),0);
-    if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-		//send sizeof
-		char * sizeof_cola_s;
-		sizeof_cola_s=intToString(strlen(cola));
-		send(s,sizeof_cola_s,sizeof(sizeof_cola_s),0);
-    	if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-		//send cola
-		send(s,cola,strlen(cola),0);
-    	if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-				//send sizeof mensaje
-				char * sizeof_mensaje_s;
-				sizeof_mensaje_s=intToString(strlen(mensaje));
-				send(s,sizeof_mensaje_s,sizeof(sizeof_mensaje_s),0);
-				if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-					//send mensaje
-					send(s,mensaje,strlen(mensaje),0);
-					if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
-							close(s);
-							return atoi(respuesta);
-					}
-					if (leido<0) {
-							respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-							close(s);
-							return atoi(respuesta);
-					}
-				}
-				if (leido<0) {
-						respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-						close(s);
-						return atoi(respuesta);
-				}
-			}
-			if (leido<0) {
-					respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-					close(s);
-					return atoi(respuesta);
-			}
-		}
-		if (leido<0) {
-				respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-				close(s);
-				return atoi(respuesta);
-		}
-	}
-	if (leido<0) {
-			respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';
-			close(s);
+	respuesta[0]='-';respuesta[1]='1';respuesta[2]='\0';ssize_t nwritten;
+	struct iovec iov[5];
+	iov[0].iov_base=opcion;
+	iov[0].iov_len=2;
+	iov[1].iov_base=sizeof_cola_s;
+	iov[1].iov_len=TAM_LONG;
+	/*
+	 AQUI EL TAMANIO VARIABLE DE LA COLA
+	*/
+	iov[2].iov_base=cola;
+	iov[2].iov_len=atoi(sizeof_cola_s)+1;
+	/*
+	AQUI SOLAMENTE HE ENVIADO UNA TRANSMISION
+	*/
+	/*
+	iov[3].iov_base="0\0";
+	iov[3].iov_len=2;
+	*/
+	char * sizeof_mensaje_s;
+	sizeof_mensaje_s=intToString((long)tam);
+	iov[3].iov_base=sizeof_mensaje_s;
+	iov[3].iov_len=TAM_LONG;
+	iov[4].iov_base=((char*)mensaje);
+	iov[4].iov_len=((int)tam)+1;
+	while((nwritten=writev(s,iov,5))>0){
+		if(read(s,respuesta,sizeof(respuesta))>0){
+			//close(s);
 			return atoi(respuesta);
+		}
 	}
-	//
 	return atoi(respuesta);
 }
 int get(const char *cola, void **mensaje, uint32_t *tam, bool blocking) {
-
-	int s=obtenerSocket();
 	int i;
+	int s=obtenerSocket();
 	//
 	char sizeof_mensaje_s[TAM_LONG];
 	//inicializar cadena
@@ -134,16 +98,18 @@ int get(const char *cola, void **mensaje, uint32_t *tam, bool blocking) {
 	int leido;
 	//send opc
 	char opcion[2];
-	opcion[0]='g';opcion[2]='\0';
-	send(s,opcion,2*sizeof(char),0);
+	if (blocking){
+		opcion[0]='b';
+	}else{
+		opcion[0]='g';
+	}
+	opcion[2]='\0';
     if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
 		//send sizeof
 		char * sizeof_cola_s;
 		sizeof_cola_s=intToString(strlen(cola));
-		send(s,sizeof_cola_s,sizeof(sizeof_cola_s),0);
     	if ((leido=read(s, respuesta,sizeof(respuesta)))>0) {
 		//send cola
-		send(s,cola,strlen(cola),0);
     	if((leido=read(s, sizeof_mensaje_s,TAM_LONG))>0) {
 			if(sizeof_mensaje_s[0]!='\000'){ 
 				//printf("sizeof_mensaje : %d\n",(int)strlen(sizeof_mensaje_s));
